9b73d0758c158c4044feebb8255a44cba052d43f
Thang322003 <thang05936@gmail.com>
2023-10-28 13:28:46 +0700

    editUser

--- chuyen-de-1/src/App.js
+++ chuyen-de-1/src/App.js
@@ -1,15 +1,27 @@
-// import logo from './logo.svg';
-import './App.scss';
-import Login from './components/Login';
-// import R from './components/test';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import EmpListing from './EmpListing';
+import EmpCreate from './EmpCreate';
+import EmpDetail from './EmpDetail';
+import EmpEdit from './EmpEdit';
 
 function App() {
   return (
-    <div className="app-containers">
-    <Login/>
-    {/* <R/> */}
+    <div className="App">
+      <h1>Quan Li Kho Hang</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<EmpListing />}></Route>
+          <Route path='/employee/create' element={<EmpCreate />}></Route>
+
+          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
+          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
+
 }
 
 export default App;
--- chuyen-de-1/src/index.js
+++ chuyen-de-1/src/index.js
@@ -2,34 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './components/Login';
-import RegisterAccount from './components/RegisterAccount'
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "login",
-    element: <Login/>,
-  },
-  {
-    path: "registeraccount",
-    element: <RegisterAccount/>,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
