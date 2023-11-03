e8d886bc9c781b2d75213af00d228808a6bc4154
NguyenHoangKinh29808 <21211tt4678@mail.tdc.edu.vn>
2023-11-04 02:15:39 +0700

    .

--- chuyen-de-1/src/components/InformationWarehouse.js
+++ chuyen-de-1/src/components/InformationWarehouse.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { useState,useEffect } from "react";
+import { BASE_URL } from "../config";
 import axios from "axios";
 const InformationWarehouse = () => {
-
-  return (
-    <>
+    let Token = localStorage.getItem("jsonwebtoken");
+    // const [columns, setColumns] = useState([]);
+    const [records, setRecords] = useState([]);
+  
+    useEffect(() => {
+      axios
+        .get(
+          BASE_URL+`/warehouse/list?id_owner=${records._id}`,
+          {
+            headers: {
+              Token:
+              Token
+            },
+          }
+        )
+        .then((res) => {
+        //   setColumns(Object, res.data);
+          setRecords(res.data.warehouses);
+          console.log(res);
+        });
+    }, []);
+  return (<>
       <div className="container d-flex justify-content-center">
         <div className="row">
         {/* <div className="image"> */}
@@ -11,22 +32,23 @@ const InformationWarehouse = () => {
         {/* </div> */}
           <h1 className="wareHouseName">
             Bán nhà xưởng và kho lạnh 100% vốn nước ngoài
+            {records.wareHouseName}
           </h1>
           <hr />
           <div className="box-monney-capacty row">
           <div className="col-1">
             <h6 className="">Mức giá</h6>
-            <div className="monney">Thỏa thuận</div>
+            <div className="monney">{records.monney}Thỏa thuận</div>
           </div>
           <div className="col-1">
           <h6 className="capacity ">Diện tích</h6>
-          <div className="capacity ">5.000 m²</div>
+          <div className="capacity ">{records.capacity}5.000 m²</div>
           </div>
           </div>
           <hr />
           <div className="row">
           <h6 className="text">Thông tin mô tả</h6>
-          <p className="description">+ Công ty TNHH Thực Phẩm & kho lạnh P. K Vietfood 100% vốn nước ngoài, cty đang hoạt động bình thường.
+          <p className="description">{records.description}+ Công ty TNHH Thực Phẩm & kho lạnh P. K Vietfood 100% vốn nước ngoài, cty đang hoạt động bình thường.
 + Giá bán thỏa thuận theo giá trị thẩm định tại thời điểm hiện tại.
 + Được phép kinh doanh sản xuất chế biến các ngành nghề về thực phẩm.
 + Vị trí: Nằm trong KCN Long Hậu, Cần Giuộc, long An.
@@ -34,7 +56,7 @@ const InformationWarehouse = () => {
           </div>
           <div className="row">
           <h5 className="">Địa Chỉ</h5>
-          <div className="address"> thành phố Hồ Chí Minh</div>
+          <div className="address"> thành phố Hồ Chí Minh {records.address}</div>
           </div>
         </div>
       </div>
