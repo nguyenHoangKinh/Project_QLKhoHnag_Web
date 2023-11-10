9c6bb49d6339630e34f0a804274d7a85d1e54620
NguyenHoangKinh29808 <21211tt4678@mail.tdc.edu.vn>
2023-11-11 00:43:59 +0700

    list Order api

--- chuyen-de-1/src/components/ShowOrders.js
+++ chuyen-de-1/src/components/ShowOrders.js
@@ -1,13 +1,15 @@
 import React from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { useState,useEffect,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { Logout } from "../context/UserContext";
 import ListGroup from "react-bootstrap/ListGroup";
-import {UserContext} from '../context/UserContext';
+import { UserContext } from "../context/UserContext";
+// import {jwtDecode}  from "jwt-decode"
 const ShowOrders = () => {
   let Token = localStorage.getItem("jsonwebtoken");
-  const {loadingData,orderList,ListOrder} = useContext(UserContext);
+
+  const { loadingData, orderList, ListOrder } = useContext(UserContext);
   useEffect(() => {
     //call api
     orderList(Token);
@@ -20,35 +22,37 @@ const ShowOrders = () => {
     <>
       <div className="category-containers">
         <ListGroup className="category-row">
-        <h1 className="text-center">List Order</h1>
+          <h1 className="text-center">List Order</h1>
           {ListOrder &&
             ListOrder.length > 0 &&
             ListOrder.map((item, index) => {
               return (
-          <ListGroup.Item className="category-item">
-            <div class="row no-gutters">
-              {/* <aside class="col-md-3 mt-2">
+                <ListGroup.Item className="category-item" >
+                  <div class="row no-gutters">
+                    {/* <aside class="col-md-3 mt-2">
                     <a href="#" class="img-wrap ">
                       <img src="https://file4.batdongsan.com.vn/2023/11/02/20231102084859-e975_wm.jpg" className="img-fluid"/>
                     </a>
                   </aside> */}
-              <div class="col-md-6">
-                <div class="info-main text-start">
-                  <h3 className="text-start"> Thông tin hóa đơn</h3>
-                    <div className="">Tên chủ kho: {ListOrder.owner}</div>
-                    <div className="">Tên kho hàng: {ListOrder.warehouses} </div>
-                    <div className="">Thời gian thuê: {ListOrder.rentalTime} </div>
-                </div>
-              </div>
-              <aside class="col-sm-6">
-                <div class="info-aside">
-                  <div class="price-wrap text-start">
-                    <span class="price h5"> Thông tin bên mua </span>
-                    <div className="">Tên khách hàng: {ListOrder.user}</div>
-                  </div>
-                  <br />
-                  <h4 className="text-center">{ListOrder.money}$ </h4>
-                  {/* <p>
+                    <div class="col-md-6">
+                      <div class="info-main text-start">
+                        <h3 className="text-start"> Thông tin hóa đơn</h3>
+                        <div className="">Tên chủ kho: {item.owner}</div>
+                        <div className="">Tên kho hàng: {item.warehouses} </div>
+                        <div className="">
+                          Thời gian thuê: {item.rentalTime}{" "}
+                        </div>
+                      </div>
+                    </div>
+                    <aside class="col-sm-6">
+                      <div class="info-aside">
+                        <div class="price-wrap text-start">
+                          <span class="price h5"> Thông tin bên mua </span>
+                          <div className="">Tên khách hàng: {item.user}</div>
+                        </div>
+                        <br />
+                        <h4 className="text-center">{item.money}$ </h4>
+                        {/* <p>
                     <a href="#" class="btn btn-primary btn-block">
                       {" "}
                       Details{" "}
@@ -58,11 +62,11 @@ const ShowOrders = () => {
                       <span class="text">Add to wishlist</span>
                     </a>
                   </p> */}
-                </div>
-              </aside>
-            </div>
-          </ListGroup.Item>
-          );
+                      </div>
+                    </aside>
+                  </div>
+                </ListGroup.Item>
+              );
             })}
         </ListGroup>
       </div>
--- chuyen-de-1/src/context/UserContext.js
+++ chuyen-de-1/src/context/UserContext.js
@@ -1,81 +1,85 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { BASE_URL } from "../config";
 import React, { createContext, useState } from "react";
-import {jwtDecode}  from "jwt-decode"
+import { jwtDecode } from "jwt-decode";
 export const UserContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [loadingData, setLoadingData] = useState(false);
-    const [ListOrder, setListOrder] = useState([]);
+  const [ListOrder, setListOrder] = useState([]);
 
-const LoginUserToken = (username,password) =>{
-  setLoadingData(true);
-  axios
-  .post(BASE_URL+"/auth/login", {
-    username: username,
-    password: password,
-  })
-  .then((response) => {
-    if (response.data) {
-      // alert("dang nhap thanh cong")
-      setLoadingData(false);
-      console.log(response.data.others);
-      const token = response.data.accessToken;
-      localStorage.setItem("jsonwebtoken", token);
-      if (response.data.others.isOwner) {
-        window.location.href = "/HomeScreen";
-      }else{
-        window.location.href = "/HomeUserScreen";
-      }
-      
-    }
-  })
-  .catch((error) => {
-    // console.log(error.request.response);
-    alert(error.request.response.slice(12).replace('"}', ""));
-  });
-};
-const Logout = (token) => {
-  console.log("category: ", token);
-  axios
-    .get(BASE_URL + "/auth/logout", {
-      headers: { Authorization: `Token ${token}` }
-    })
-    .then((res) => {
-      if (res) {
-        alert(res.data.message);
-        localStorage.removeItem("jsonwebtoken");
-        window.location.href = "/login";
-      }
-    }).catch((error) => {
-      alert(error.request.response.slice(12).replace('"}', ""));
+  const LoginUserToken = (username, password) => {
+    setLoadingData(true);
+    axios
+      .post(BASE_URL + "/auth/login", {
+        username: username,
+        password: password,
+      })
+      .then((response) => {
+        if (response.data) {
+          // alert("dang nhap thanh cong")
+          setLoadingData(false);
+          console.log(response.data.others);
+          const token = response.data.accessToken;
+          localStorage.setItem("jsonwebtoken", token);
+          if (response.data.others.isOwner) {
+            window.location.href = "/HomeScreen";
+          } else {
+            window.location.href = "/HomeUserScreen";
+          }
+        }
+      })
+      .catch((error) => {
+        // console.log(error.request.response);
+        alert(error.request.response.slice(12).replace('"}', ""));
       });
+  };
+  const Logout = (token) => {
+    console.log("category: ", token);
+    axios
+      .get(BASE_URL + "/auth/logout", {
+        headers: { Authorization: `Token ${token}` },
+      })
+      .then((res) => {
+        if (res) {
+          alert(res.data.message);
+          localStorage.removeItem("jsonwebtoken");
+          window.location.href = "/login";
+        }
+      })
+      .catch((error) => {
+        alert(error.request.response.slice(12).replace('"}', ""));
+      });
+  };
+  const orderList = (Token) => {
+    let idUser = jwtDecode(Token);
+    axios
+      .get(BASE_URL + `/order/listOrderByUser?id_user=${idUser.id}`, {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
+      })
+      .then((res) => {
+        if (res && res.data) {
+          setListOrder(res.data);
+        }
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+  return (
+    <UserContext.Provider
+      value={{
+        ListOrder,
+        loadingData,
+        orderList,
+        Logout,
+        LoginUserToken,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
-const orderList = (Token) => {
-  let idUser= jwtDecode(Token)
-  axios.get(BASE_URL+`/order/listOrderByOwner?id_user=${idUser.id}`, {
-    headers: { 
-      Authorization: `Bearer ${Token}` 
-    }
-  }).then((res) => {
-  if (res && res.data && res.data) {
-    setListOrder(res.data);
-   }
-  }).catch((error)=>{
-  alert(error.message);
-  });
-}
-return (
-  <UserContext.Provider
-    value={{
-      ListOrder,
-      loadingData,
-      orderList,
-      Logout,
-      LoginUserToken,
-    }}
-  >
-    {children}
-  </UserContext.Provider>
-);
-}
