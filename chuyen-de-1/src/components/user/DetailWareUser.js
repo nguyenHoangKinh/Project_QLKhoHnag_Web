4a9fe30f06865cc0a3695a69de9dc2094b59f01b
NguyenVoBaoPhi <phipk456@gamil.com>
2023-12-21 16:01:11 +0700

    sửa lại thuê theo tháng và giao diện

--- chuyen-de-1/src/components/user/DetailWareUser.js
+++ chuyen-de-1/src/components/user/DetailWareUser.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { Link } from "react-router-dom";
+import "../../theme/RentWareHouse.css";
 
 const DetailWareUser = () => {
   let Token = localStorage.getItem("jsonwebtoken");
@@ -22,44 +23,49 @@ const DetailWareUser = () => {
   };
 
   const handleStartDateChange = (event) => {
-    setStartDate(new Date(event.target.value));
+    setStartDate(event.target.value);
   };
 
   const handleEndDateChange = (event) => {
-    const selectedEndDate = new Date(event.target.value);
-    const daysToRent = Math.ceil((selectedEndDate - startDate) / (1000 * 60 * 60 * 24));
+    setEndDate(event.target.value);
+    const daysToRent = Math.ceil(
+      (new Date(event.target.value + "-01") - new Date(startDate + "-01")) /
+        (1000 * 60 * 60 * 24)
+    );
     setRentalTime(daysToRent);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     axios
-    .post(
-      `https://warehouse-management-api.vercel.app/v1/order/create`,
-      {
-        capacity: capacity,
-        rentalTime: rentalTime,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${Token}`,
+      .post(
+        `https://warehouse-management-api.vercel.app/v1/order/create`,
+        {
+          capacity: capacity,
+          rentalTime: rentalTime,
+          startDate: startDate, // Truyền giá trị ngày bắt đầu là tháng và năm
+          endDate: endDate, // Truyền giá trị ngày kết thúc là tháng và năm
         },
-        params: {
-          id_warehouse: id,
-        },
-      }
-    )
-    .then((res) => {
-      //console.log(res.data.data);  // In ra dữ liệu trả về từ API để kiểm tra cấu trúc
-      let capacity = res.data;
-      // console.log(userInfo.accessToken);
-      const orderId = res.data.data._id.toString(); // Chuyển đổi thành chuỗi nếu cần
-      // // Sử dụng orderId mà không gặp lỗi
-      alert("gui thanh cong");
-    })
-    .catch((e) => {
-      console.log(`error ${e.response.data.message}`);
-    });
+        {
+          headers: {
+            Authorization: `Bearer ${Token}`,
+          },
+          params: {
+            id_warehouse: id,
+          },
+        }
+      )
+      .then((res) => {
+        //console.log(res.data.data);  // In ra dữ liệu trả về từ API để kiểm tra cấu trúc
+        let capacity = res.data;
+        // console.log(userInfo.accessToken);
+        const orderId = res.data.data._id.toString(); // Chuyển đổi thành chuỗi nếu cần
+        // // Sử dụng orderId mà không gặp lỗi
+        alert("gui thanh cong");
+      })
+      .catch((e) => {
+        console.log(`error ${e.response.data.message}`);
+      });
     // try {
     //   // Gửi yêu cầu đến API khi có đủ thông tin
     //   if (capacity && rentalTime > 0) {
@@ -77,7 +83,7 @@ const DetailWareUser = () => {
     //           id_warehouse: id,
     //         },
     //     }
-          
+
     //     //   {
     //     //     id_warehouse: id,
     //     //     capacity: capacity,
@@ -97,38 +103,43 @@ const DetailWareUser = () => {
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="form-container">
         <label>
           Dung tích (%):
           <select value={capacity} onChange={handleCapacityChange}>
-            <option value={'0%'}>0%</option>
-            <option value={'25%'}>25%</option>
-            <option value={'50%'}>50%</option>
-            <option value={'75%'}>75%</option>
-            <option value={'100%'}>100%</option>
+            <option value={"0%"}>0%</option>
+            <option value={"25%"}>25%</option>
+            <option value={"50%"}>50%</option>
+            <option value={"75%"}>75%</option>
+            <option value={"100%"}>100%</option>
           </select>
         </label>
         <br />
         <label>
-          Ngày bắt đầu:
-          <input type="date" value={startDate.toISOString().split("T")[0]} onChange={handleStartDateChange} />
+          Tháng và năm bắt đầu:
+          <input
+            type="month"
+            value={startDate}
+            onChange={handleStartDateChange}
+            className="date-input"
+          />
         </label>
         <br />
         <label>
-          Ngày kết thúc:
-          <input type="date" onChange={handleEndDateChange} />
+          Tháng và năm kết thúc:
+          <input type="month" onChange={handleEndDateChange} className="date-input"/>
         </label>
         <br />
         <button type="submit">Gửi yêu cầu</button>
       </form>
-       {/* Hiển thị thông báo nếu có */}
-       {message && (
-        <div>
+      {/* Hiển thị thông báo nếu có */}
+      {message && (
+        <div className="message-container">
           <h3>Thông báo:</h3>
           <p>{message}</p>
         </div>
       )}
-      <Link to="javascript:history.go(-1)">Quay lại</Link>
+      <Link to="javascript:history.go(-1)" className="back-link">Quay lại</Link>
     </div>
   );
 };
