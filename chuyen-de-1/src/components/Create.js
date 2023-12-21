f3f66c721dc2d03486bd9006f2b6c28e41acebde
NguyenVoBaoPhi <phipk456@gamil.com>
2023-12-21 15:24:19 +0700

    sua lai them kho hang

--- chuyen-de-1/src/components/Create.js
+++ chuyen-de-1/src/components/Create.js
@@ -1,15 +1,11 @@
-import React, { useState } from "react";
-import { addWare } from "../WareReducer";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { BASE_URL } from "../config";
-import {jwtDecode}  from "jwt-decode"
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 function Create() {
-  let Token = localStorage.getItem("jsonwebtoken");
-  let idUser= jwtDecode(Token)
-  
+  const Token = localStorage.getItem('jsonwebtoken');
+  const idUser = jwtDecode(Token);
+
   const [inputData, setInputData] = useState({
     wareHouseName: '',
     address: '',
@@ -18,120 +14,188 @@ function Create() {
     monney: '',
     status: '',
     description: '',
-    owner: idUser.id
+    owner: idUser.id,
+    imageWarehouse: '',
   });
-  const navigate = useNavigate();
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://warehouse-management-api.vercel.app/v1/warehouse/category/list', {
+        headers: {
+          Authorization: `Bearer ${Token}`,
+        },
+      })
+      .then((res) => {
+        const categoriesData = res.data.categories;
+        setCategories(categoriesData);
+      })
+      .catch((error) => {
+        console.error(`Error fetching categories: ${error}`);
+      });
+  }, []);
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setInputData({ ...inputData, imageWarehouse: reader.result });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 
   function handleSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
     axios
       .post(
-        BASE_URL+`/warehouse/create`,
+        'https://warehouse-management-api.vercel.app/v1/warehouse/create',
         inputData,
         {
           headers: {
-            Authorization: `Token ${Token}` 
+            Authorization: `Token ${Token}`,
           },
           params: {
-            id_owner: idUser.id
-          }
+            id_owner: idUser.id,
+          },
         }
       )
       .then((res) => {
-        //setInputData(Object, res.data)
-        console.log(res);
-        alert("Them thanh cong");
-        navigate("/HomeScreen");
+        console.log(res.data);
+        alert('Thêm thành công');
+        //navigate('/HomeScreen');
       })
       .catch((err) => console.log(err));
   }
 
   return (
-    <div className="d-flex w-100 vh-100 justify-content-center align-items-center">
-      <div className="w-50 border bg-light p-5">
-        <h3>Them Kho Moi</h3>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="wareHouseName">Ten:</label>
-            <input
-              type="text"
-              name="wareHouseName"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, wareHouseName : e.target.value })
-              }
-            />
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card p-4">
+            <h3 className="mb-4 text-center">Thêm Kho Mới</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="wareHouseName" className="form-label">
+                  Tên Kho:
+                </label>
+                <input
+                  type="text"
+                  name="wareHouseName"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({
+                      ...inputData,
+                      wareHouseName: e.target.value,
+                    })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="address" className="form-label">
+                  Địa chỉ:
+                </label>
+                <input
+                  type="text"
+                  name="address"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, address: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="category" className="form-label">
+                  Danh mục:
+                </label>
+                <select
+                  name="category"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({
+                      ...inputData,
+                      category: e.target.value,
+                    })
+                  }
+                >
+                  <option value="">Chọn Danh mục</option>
+                  {categories.map((cat) => (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="capacity" className="form-label">
+                  Dung Tích:
+                </label>
+                <input
+                  type="text"
+                  name="capacity"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, capacity: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="monney" className="form-label">
+                  Tiền:
+                </label>
+                <input
+                  type="text"
+                  name="monney"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, monney: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="status" className="form-label">
+                  Trạng Thái:
+                </label>
+                <input
+                  type="text"
+                  name="status"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, status: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="description" className="form-label">
+                  Mô Tả:
+                </label>
+                <input
+                  type="text"
+                  name="description"
+                  className="form-control"
+                  onChange={(e) =>
+                    setInputData({ ...inputData, description: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="imageWarehouse" className="form-label">
+                  Upload Ảnh:
+                </label>
+                <input
+                  type="file"
+                  name="imageWarehouse"
+                  accept="image/*"
+                  className="form-control-file"
+                  onChange={handleFileChange}
+                />
+                </div>
+              <button className="btn btn-info">Submit</button>
+            </form>
           </div>
-          <div>
-            <label htmlFor="address">Dia chi:</label>
-            <input
-              type="text"
-              name="address"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, address: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="category">Danh muc</label>
-            <input
-              type="text"
-              name="category"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, category: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="capacity">Dung Tich</label>
-            <input
-              type="text"
-              name="capacity"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, capacity: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="monney">Tien</label>
-            <input
-              type="text"
-              name="monney"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, monney: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="status">Trang Thai</label>
-            <input
-              type="text"
-              name="status"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, status: e.target.value })
-              }
-            />
-          </div>
-          <div>
-            <label htmlFor="description">Mo ta</label>
-            <input
-              type="text"
-              name="description"
-              className="form-control"
-              onChange={(e) =>
-                setInputData({ ...inputData, description: e.target.value })
-              }
-            />
-          </div>
-          
-          <br />
-          <button className="btn btn-info">Submit</button>
-        </form>
+        </div>
       </div>
     </div>
   );
