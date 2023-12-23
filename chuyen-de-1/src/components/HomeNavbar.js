1cfa2643780bb1a0542cf5f9c501608b7ba87b99
Shjnro <21211TT3483@mail.tdc.edu.vn>
2023-12-23 15:47:02 +0700

    fix navbar

--- chuyen-de-1/src/components/HomeNavbar.js
+++ chuyen-de-1/src/components/HomeNavbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     const [user, setUser] = useState();
     let token = localStorage.getItem("jsonwebtoken");
     let idUser;
-    
+
     if (token) {
         idUser = jwtDecode(token);
     }
@@ -55,26 +55,6 @@ const Navbar = () => {
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item me-2">
                                 <a className="nav-link text-uppercase fs-5 active text-white" href="/">Home</a>
-                            </li>
-                            <li className="nav-item mx-2 dropdown">
-                                <a className="nav-link text-uppercase fs-5 dropdown-toggle text-white" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider" />
-                                    </li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li className="nav-item mx-2">
-                                <a className="nav-link text-uppercase fs-5 text-white" href="#">Link</a>
-                            </li>
-                            <li className="nav-item mx-2">
-                                <a className="nav-link text-uppercase fs-5 text-white" href="#">Link</a>
                             </li>
                         </ul>
                         <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
@@ -132,22 +112,14 @@ const Navbar = () => {
                             <li className="nav-item me-2">
                                 <a className="nav-link text-uppercase fs-5 active text-white" href="/">Home</a>
                             </li>
-                            <li className="nav-item mx-2 dropdown">
-                                <a className="nav-link text-uppercase fs-5 dropdown-toggle text-white" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Quản Lý Đơn Hàng
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider" />
-                                    </li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                            <li className="nav-item mx-2">
+                                <a className="nav-link text-uppercase fs-5 text-white" onClick={orderOwner}>Danh sách đơn hàng</a>
                             </li>
                             <li className="nav-item mx-2">
-                                <a className="nav-link text-uppercase fs-5 text-white" onClick={orderOwner}>List Order</a>
+                                <a className="nav-link text-uppercase fs-5 text-white" href="ListPostOwner">Quản Lý Bài Viết</a>
+                            </li>
+                            <li className="nav-item mx-2">
+                                <a className="nav-link text-uppercase fs-5 text-white" href="ListWarehouseOwner">Quản Lý Kho Hàng</a>
                             </li>
                             <li className="nav-item mx-2">
                                 <a className="nav-link text-uppercase fs-5 text-white" onClick={ChatUser}>Chat</a>
@@ -170,32 +142,16 @@ const Navbar = () => {
                             <li className="nav-item me-2">
                                 <a className="nav-link text-uppercase fs-5 active text-white" href="/">Home</a>
                             </li>
-                            <li className="nav-item mx-2 dropdown">
-                                <a className="nav-link text-uppercase fs-5 dropdown-toggle text-white" href="#" role="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">Action</a></li>
-                                    <li><a className="dropdown-item" href="#">Another action</a></li>
-                                    <li>
-                                        <hr className="dropdown-divider" />
-                                    </li>
-                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
+                            <li className="nav-item mx-2">
+                                <a className="nav-link text-uppercase fs-5 text-white" onClick={orderUser}>Danh sách đơn hàng</a>
                             </li>
                             <li className="nav-item mx-2">
                                 <a className="nav-link text-uppercase fs-5 text-white" href="ListWarehouseUser">Danh Sách Kho Hàng</a>
                             </li>
-                                                        <li className="nav-item mx-2">
-                                <a className="nav-link text-uppercase fs-5 text-white" onClick={orderUser}>orderUser</a>
-                            </li>
                             <li className="nav-item mx-2">
                                 <a className="nav-link text-uppercase fs-5 text-white" onClick={ChatUser}>chat</a>
                             </li>
-                            <li className="nav-item mx-2">
-                                <a className="nav-link text-uppercase fs-5 text-white" href="#">Link</a>
-                            </li>
+                           
                         </ul>
                         <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                             <li className="nav-item mx-2">
